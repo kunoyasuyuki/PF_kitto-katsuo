c514ca8c29b14a32c32c480608dc22b8d288c240
kunoyasu <kunoyasuyuki@yahoo.co.jp>
2020-09-09 13:39:57 +0900

    ルーティング設計

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
-  get 'tickets/index'
-  devise_for :users
+  root "tickets#index"
+  devise_for :users, controllers: { registrations: 'users/registrations' }
+   resources :users, only: [:edit, :update,:destroy]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
