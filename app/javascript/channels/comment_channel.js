634fb4f0d43d7f0b47adee05f52c2c0f643b857d
kunoyasu <kunoyasuyuki@yahoo.co.jp>
2020-09-18 18:09:16 +0900

    コメントビューの修正

--- app/javascript/channels/comment_channel.js
+++ app/javascript/channels/comment_channel.js
@@ -10,12 +10,38 @@ consumer.subscriptions.create("CommentChannel", {
   },
 
   received(data) {
-    const html = `<p>${data.user.nickname}</p>`;
-    const html2 = `<p>${data.content.text}</p>`;
+    const html =    
+    `<div class="upper-message">
+     <div class="message-user">
+       ${data.user.nickname}
+     </div>
+     <div class="message-date">
+       ${data.content.created_at}
+      </div>
+   </div>
+   <div class="lower-message">
+     <div class="message-content">
+       ${data.content.text}
+     </div>
+    </div>`;
     const messages = document.getElementById('messages');
     const newMessage = document.getElementById('comment_text');
     messages.insertAdjacentHTML('afterbegin', html);
-    messages.insertAdjacentHTML('afterbegin', html2);
     newMessage.value='';
+
+
+
+
+  // received(data) {
+  //   const html = `<p>${data.user.nickname}</p>`;
+  //   const html2 = `<p>${data.content.text}</p>`;
+  //   const html3 = `<p>${comment.created_at}</p>`;
+  //   const messages = document.getElementById('messages');
+  //   const newMessage = document.getElementById('comment_text');
+  //   messages.insertAdjacentHTML('afterbegin', html);
+  //   messages.insertAdjacentHTML('afterbegin', html2);
+  //   messages.insertAdjacentHTML('afterbegin', html3);
+  //   newMessage.value='';
   }
 });
+
