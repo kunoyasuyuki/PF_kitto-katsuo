4ff20a4ef17ca3edf3f362ca52d00b74f3b14c35
kunoyasu <kunoyasuyuki@yahoo.co.jp>
2020-09-22 10:26:25 +0900

    cable.yml

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -3,6 +3,9 @@ class CommentsController < ApplicationController
 
   def new
   end
+
+
+
   
   def create
    @comment = Comment.new(comment_params)
--- app/controllers/rooms_controller.rb
+++ app/controllers/rooms_controller.rb
@@ -3,11 +3,10 @@ class RoomsController < ApplicationController
 
    def index
     @rooms = Room.where(user_id: current_user.id)
+    ticket_id = Ticket.find(ticket_id)
     # @room = Room.find(params[:room_id])
     # @messages = @room.messages.includes(:user)
    end
-
-
   
     def create
         @ticket = Ticket.find(params[:ticket_id])
@@ -15,40 +14,22 @@ class RoomsController < ApplicationController
         @currentRoomUser = RoomUser.where(user_id: current_user.id)
         @RoomUserEntry = RoomUser.where(user_id: @user.id)
         @currentRoomUser.each do |cu|
-            @RoomUserEntry.each do |u|
-                if cu.room_id == u.room_id 
+         @RoomUserEntry.each do |u|
+           if cu.room_id == u.room_id 
                     # @room.ticket.id = @ticket.room_id 
                     @isRoom = true
-                end
             end
+          end
         end
         unless @isRoom
             @room = Room.new(name: params[:name], ticket_id: params[:ticket_id])
-            if @room.save
+         if @room.save
                 RoomUser.create(room_id: @room.id, user_id: current_user.id)
                 RoomUser.create(room_id: @room.id, user_id: Ticket.find(params[:ticket_id]).user_id)
                 redirect_to  user_room_messages_path(Ticket.find(params[:ticket_id]).user_id, @room.id)
-            else
+         else
                 render :create
-            end
+         end
         end
     end
 end
-
-
-
-# @ticket = Ticket.find(params[:ticket_id])
-# @user = User.find(user_id: current_user.id)
-#   @currentRoomUser = RoomUser.where(user_id: current_user.id)
-#   @RoomUserEntry   = RoomUser.where(user_id:  @user.id)
-#     @currentRoomUser.each do |cu|
-#     @RoomUserEntry .each do |u|
-#          if cu.room_id == u.room_id 
-#         @room.ticket.id== ticket_id.room_id 
-#         @isRoom = true
-#         @roomId = cu.room_id
-#   end
-#    end
-#     end
-
-#     unless @isRoom
